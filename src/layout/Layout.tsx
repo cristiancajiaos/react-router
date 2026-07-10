beff605978097c896a7a29a2c5aaf139dc0cff94
Cristian Cajiao <cristiancajiaos@outlook.com>
2026-07-10 15:30:46 -0400

    Creada vista Products

--- src/layout/Layout.tsx
+++ src/layout/Layout.tsx
@@ -3,18 +3,21 @@ import './Layout.css'
 import Home from '../views/home/Home'
 import About from '../views/about/About'
 import Contact from '../views/contact/Contact'
+import Products from '../views/products/Products'
 
 export default function Layout() {
   return (
     <BrowserRouter>
       <nav>
         <Link to="/">Home</Link> | {" "}
+        <Link to="/products">Products</Link> | {" "}
         <Link to="/about">About</Link> |{ " "} 
         <Link to="/contact">Contact</Link>
       </nav>
 
       <Routes>
         <Route path="/" element={<Home/>}/>
+        <Route path="/products" element={ <Products/>}/>
         <Route path="/about" element={ <About/>}/>
         <Route path="/contact" element={ <Contact/>}/>
       </Routes>
